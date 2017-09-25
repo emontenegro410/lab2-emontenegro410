c7fae0c1a0f30d1404c67a773b2cd0116deca580
Eunice Montenegro <emontenegro1@luc.edu>
2017-09-25 06:41:47 +0000

    Update
    Signed-off-by: Eunice Montenegro <emontenegro1@luc.edu>

--- src/test/java/edu/luc/cs/cs271/lab2/TestSearch.java
+++ src/test/java/edu/luc/cs/cs271/lab2/TestSearch.java
@@ -15,28 +15,73 @@ public class TestSearch {
   }
 
   // TODO makeListFixture
+  
+    List<Team> makeListFixture(final int size) {
+    final Team[] array = new Team[size];
+    final List<Team> list = Arrays.asList(array);
+       for (int i = 0; i < size; i++) {
+         final String s = Integer.toString(i);
+            array[i] = new Team("Team " + s, "Coach " + s, i * 100 + 50);
+         }
+    return list;
+  }
+  
+
+ // Done TODO: testFindPositionList0, 10s, 10f
+    @Test
+  public void testFindPositionList0() {
+    final List<Team> = makeListFixture(0);
+    assertFalse(Search.findTeamPosition(List, "Team 2").isPresent()); //similar search to array 
+  }
 
   @Test
-  public void testFindPositionArray0() {
+  public void testFindPositionList10s() {
+    final List<Team> = makeListFixture(10);
+    assertTrue(Search.findTeamPosition(List, "Team 5").isPresent());
+  }
+
+  @Test
+  public void testFindPositionList10f() {
+    final List <Team>= makeListFixture(10);
+    assertFalse(Search.findTeamPosition(List "Team 10").isPresent());
+  }
+  
+  // Done testFindMinFundingArray for several sizes and scenarios
+     @Test
+  public void testFindMinFundingArray0() {
     final Team[] arr = makeArrayFixture(0);
-    assertFalse(Search.findTeamPosition(arr, "Team 5").isPresent());
+    assertFalse(Search.findTeamMinFunding(arr, 7).isPresent());
+  }
+  
+   @Test
+  public void testFindMinFundingArray10s() {
+    final Team[] arr = makeArrayFixture(10);
+    assertTrue(Search.findTeamMinFunding(arr, 25).isPresent());
+  }
+  
+   @Test
+  public void testFindMinFundingArray10f() {
+    final Team[] arr = makeArrayFixture(10);
+    assertFalse(Search.findTeamMinFunding(arr, 50).isPresent());
+  }
+
+  // Done TODO: testFindMinFundingArrayFast for several sizes and scenarios
+
+   @Test
+  public void testFindMinFundingArrayFast0() {
+    final Team[] arr = makeArrayFixture(0);
+    assertFalse(Search.findTeamMinFundingFast(arr, 500).isPresent());
   }
 
   @Test
-  public void testFindPositionArray10s() {
+  public void testFindMinFundingArrayFast10s() {
     final Team[] arr = makeArrayFixture(10);
-    assertTrue(Search.findTeamPosition(arr, "Team 5").isPresent());
+    assertTrue(Search.findTeamMinFundingFast(arr,500).isPresent());
   }
 
   @Test
-  public void testFindPositionArray10f() {
+  public void testFindMinFundingArrayFast10f() {
     final Team[] arr = makeArrayFixture(10);
-    assertFalse(Search.findTeamPosition(arr, "Team 11").isPresent());
+    assertFalse(Search.findTeamMinFundingFast(arr, 1000).isPresent());
   }
-  
-  // TODO: testFindPositionList0, 10s, 10f
-  
-  // TODO: testFindMinFundingArray for several sizes and scenarios
-
-  // TODO: testFindMinFundingArrayFast for several sizes and scenarios
 }
--- src/test/java/edu/luc/cs/cs271/lab2/TestTeam.java
+++ src/test/java/edu/luc/cs/cs271/lab2/TestTeam.java
@@ -19,9 +19,30 @@ public class TestTeam {
     }
   }
   
-  // TODO testConstructorValidHeadcoach
-  // TODO testConstructorValidFunding
+  // Done TODO testConstructorValidHeadcoach
+  @Test
+  public void testConstructorValidHeadcoach() {
+    try {
+      new Team("USA", null, 500);
+      fail("should have thrown IllegalArgumentException");
+    } catch (final Throwable ex) {
+    }
+  }
 
+  
+  // Done TODO testConstructorValidFunding
+  @Test
+  public void testConstructorValidFunding() {
+    try {
+      new Team("USA", "Klinsmann", 0);
+      fail("should have thrown IllegalArgumentException");
+    } catch (final Throwable ex) {
+      // if we landed here, we're good!
+    }
+  }
+  
+  
+//Test for GetName
   @Test
   public void testGetName() {
     final String name = "USA";
@@ -29,6 +50,18 @@ public class TestTeam {
     assertEquals(name, t.getName());
   }
   
-  // TODO testGetHeadcoach
-  // TODO testGetFunding
-}
+  // Done TODO testGetHeadcoach
+  @Test
+  public void testGetHeadcoach() {
+    final String headcoach = "Klinsmann";
+    final Team t = makeTeamFixture("USA", headcoach, 500);
+    assertEquals(headcoach, t.getHeadcoach());
+  }
+
+  // Done TODO testGetFunding
+  @Test
+  public void testGetFunding() {
+    final int funding = 500;
+    final Team t = makeTeamFixture("USA", "Klinsmann", funding);
+    assertEquals(funding, t.getFunding());
+  }
